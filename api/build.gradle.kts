e006cb26aef5eab8a5efabae0d30654a1b304980
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-09-01 16:35:39 +0300

    Add checker-qual

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,6 +6,7 @@ val commonsIoVersion = "2.6"
 val jsonSimpleVersion = "1.1.1"
 val ztZipVersion = "1.13"
 val kyoriNbtVersion = "1.12-1.0.0-SNAPSHOT"
+val checkerQualVersion = "2.5.4"
 
 
 dependencies {
@@ -13,4 +14,5 @@ dependencies {
     api("com.googlecode.json-simple:json-simple:$jsonSimpleVersion")
     api("org.zeroturnaround:zt-zip:$ztZipVersion")
     api("net.kyori:nbt:$kyoriNbtVersion")
+    api("org.checkerframework:checker-qual:$checkerQualVersion")
 }
