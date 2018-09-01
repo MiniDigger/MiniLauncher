c8d52c6a0ebba5cc07d262a593bc7302eb18c2e5
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-09-01 16:17:45 +0300

    Convert to Gradle, add license headers

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.2.61"
+    id("net.minecrell.licenser") version "0.3"
+    id("com.github.johnrengelman.shadow") version "2.0.4"
+}
+
+allprojects {
+    group = "me.minidigger"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        jcenter()
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://jitpack.io")
+    }
+}
+
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "net.minecrell.licenser")
+    apply(plugin = "com.github.johnrengelman.shadow")
+
+    license {
+        header = rootProject.file("LICENSE")
+        filter.include("**/*.java")
+        filter.include("**/*.kt")
+    }
+}
